c4d8e2082e36d76bf3a56e10fc18fb059e25a48a
John Nettleton <john.nettleton@awardco.com>
2025-09-16 15:46:39 -0600

    refactor: standardize code formatting and update excluded users handling for linux

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //bolt is slack's sdk
-const { App, LogLevel } = require("@slack/bolt");
+const { App, LogLevel } = require('@slack/bolt');
 //dotenv is a library that places env variables onto the process object
-require('dotenv').config()
+require('dotenv').config();
 //you need your own .env file with the following variables
-const { BOT_TOKEN, SECRET, CHANNEL, EXCLUDE } = process.env
+const { BOT_TOKEN, SECRET, CHANNEL, EXCLUDED_USERS } = process.env;
 
 // useful user ids
 // U019045NWD6 Jeremy Jensen
@@ -11,118 +11,123 @@ const { BOT_TOKEN, SECRET, CHANNEL, EXCLUDE } = process.env
 // U02LKHB0HA7 Jarrod Ribble
 // U0271KE1LHL Cameron Zollinger
 
-// in the exclude env variable, user ids are separated by a space
-let rawExcludeIds = EXCLUDE?.split(',') ?? []
+// Define channels first
+let channels = CHANNEL?.split(' ');
+
+// Parse excluded users
+// The EXCLUDED_USERS env variable contains user IDs separated by spaces
+const excludedUsers = EXCLUDED_USERS?.split(' ') ?? [];
 const excludeIds = {};
-if (rawExcludeIds.length > 0) {
-    rawExcludeIds.forEach(e => {
-        let [channel, ids] = e.split(':')
-        ids = ids.split(' ')
-        excludeIds[channel] = ids
-    })
+
+// Initialize the excludeIds for each channel
+if (channels && channels.length > 0) {
+    channels.forEach(channel => {
+        excludeIds[channel] = excludedUsers;
+    });
 }
 
-let channels = CHANNEL?.split(' ')
+//create a new app with the bot token and signing secret
 
 //create a new app with the bot token and signing secret
 const app = new App({
-    token: BOT_TOKEN,
-    signingSecret: SECRET,
-    LogLevel: LogLevel.DEBUG
-})
+	token: BOT_TOKEN,
+	signingSecret: SECRET,
+	LogLevel: LogLevel.DEBUG,
+});
 
 //start the app
 const doIt = async () => {
-    for (channel of channels) {
-        //get member ids from channel
-        let members = await getUsers(channel)
-        //get names from member ids
-        let names = await getName(members, channel)
-        names = shuffle(names)
-        printRotation(names, channel)
-    }
-}
-
+	for (channel of channels) {
+		//get member ids from channel
+		let members = await getUsers(channel);
+		//get names from member ids
+		let names = await getName(members, channel);
+		names = shuffle(names);
+		printRotation(names, channel);
+	}
+};
 
 const shuffle = (array) => {
-    let currentIndex = array.length, randomIndex;
+	let currentIndex = array.length,
+		randomIndex;
 
-    // while there remain elements to shuffle
-    while (currentIndex != 0) {
+	// while there remain elements to shuffle
+	while (currentIndex != 0) {
+		// pick a remaining element
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
 
-        // pick a remaining element
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
+		// swap it with the current element
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
 
-        // swap it with the current element
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
-}
+	return array;
+};
 
 const getUsers = async (channel) => {
-    const { members } = await app.client.conversations.members({
-        channel,
-        token: BOT_TOKEN
-    })
-    return members
-}
+	const { members } = await app.client.conversations.members({
+		channel,
+		token: BOT_TOKEN,
+	});
+	return members;
+};
 
 const getName = async (members, channel) => {
-    let names = []
-    //# get name of corresponding userid
-    for (let i = 0; i < members.length; i++) {
-        const name = await app.client.users.info({
-            token: BOT_TOKEN,
-            user: members[i]
-        })
-        //# ignore user if it's a bot or if it's in the list of excluded users in the .env
-        if (!name.user.is_bot && !excludeIds[channel]?.includes(name.user.id)) {
-            names.push(`<@${name.user.id}>`)
-        }
-    }
-    //? not sure i need this await?
-    return await names
-}
+	let names = [];
+	//# get name of corresponding userid
+	for (let i = 0; i < members.length; i++) {
+		const name = await app.client.users.info({
+			token: BOT_TOKEN,
+			user: members[i],
+		});
+		//# ignore user if it's a bot or if it's in the list of excluded users in the .env
+		if (!name.user.is_bot && !excludeIds[channel]?.includes(name.user.id)) {
+			names.push(`<@${name.user.id}>`);
+		}
+	}
+	//? not sure i need this await?
+	return await names;
+};
 
 const printRotation = async (names, channel) => {
-    let text = []
-    for (let i = 0; i < names.length; i++) {
-        if (i < names.length - 1) {
-            text.push({
-                type: 'section',
-                fields: [{
-                    type: 'mrkdwn',
-                    text: `${names[i]} assigns to ${names[i + 1]}`
-                }]
-            })
-        } else {
-            text.push({
-                type: 'section',
-                fields: [{
-                    type: 'mrkdwn',
-                    text: `${names[i]} assigns to ${names[0]}`
-                }]
-            })
-        }
-    }
-    // uncomment below to print randomized list to console
-    // text.forEach(e => console.log(e.fields))
-    // comment out below to avoid spamming chat when testing
-    await app.client.chat.postMessage({
-        channel,
-        token: BOT_TOKEN,
-        text: 'Peer Review Rotation',
-        blocks: text
-    })
-}
+	let text = [];
+	for (let i = 0; i < names.length; i++) {
+		if (i < names.length - 1) {
+			text.push({
+				type: 'section',
+				fields: [
+					{
+						type: 'mrkdwn',
+						text: `${names[i]} assigns to ${names[i + 1]}`,
+					},
+				],
+			});
+		} else {
+			text.push({
+				type: 'section',
+				fields: [
+					{
+						type: 'mrkdwn',
+						text: `${names[i]} assigns to ${names[0]}`,
+					},
+				],
+			});
+		}
+	}
+	// uncomment below to print randomized list to console
+	// text.forEach(e => console.log(e.fields))
+	// comment out below to avoid spamming chat when testing
+	await app.client.chat.postMessage({
+		channel,
+		token: BOT_TOKEN,
+		text: 'Peer Review Rotation',
+		blocks: text,
+	});
+};
 
 try {
-    doIt()
-}
-catch (err) {
-    console.error(err)
-    console.error(channels, excludeIds)
+	doIt();
+} catch (err) {
+	console.error(err);
+	console.error(channels, excludeIds);
 }
